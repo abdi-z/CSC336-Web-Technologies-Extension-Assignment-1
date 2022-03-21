8d770e4a9052023b9daac507a8891123ab29adfc
Abdul Rehman Aziz <fa19-bcs-016@cuilahore.edu.pk>
2022-03-21 23:20:03 +0500

    jquery funcs, and other final touches

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-// $(document).ready(function () {
-//   $("button").click(function () {
-//     $("#test").hide();
-//   });
-// });
+$(document).ready(function(){
+    let d = new Date();
+ 
+    function datefunc() {
+    document.getElementById("date").innerHTML = `${d.getDate()} / ${
+    d.getMonth() + 1
+    }/${d.getFullYear()}`;
+
+
+    }
+    $("button").click(function () {datefunc()});
+});
